1cd9b86937a0c36d16871e523b6793548a8a5332
Ali Badr <ali.hbadr0@gmail.com>
2018-03-26 16:56:44 -0400

    Update main.py
    Added the following functions:
    
    portfolio_combinations - given the securities of interest, this function will generate a data frame of different combinations n along with their respective Volatility, Sharpe Ratio and expected returns
    
    optimize_portfolio - Will analyze portfolio combinations and return the portfolio with the highest Sharpe Ratio or Lowest Volatility depending on the optimization parameter

--- main.py
+++ main.py
@@ -4,13 +4,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 from get_data import *
 
-#-----------------------------------------------------------------
-start = '2014-1-1'
-end = '2016-12-31'
-securities = ['CNP', 'F', 'WMT', 'GE', 'TSLA']
+def portfolio_combinations(data,securities,num_portfolios):
 
+    returns_daily = data.pct_change()
+    returns_annual = returns_daily.mean() * 250
+    cov_daily = returns_daily.cov()
+    cov_annual = cov_daily * 250
 
-#-----------------------------------------------------------------
+    port_returns = []
+    port_volatility = []
+    stock_weights = []
+    sharpe_ratio = []
+    num_assets = len(securities)
+    num_portfolios = 50000
+    portfolio = {'Returns': port_returns,
+        'Volatility': port_volatility,
+        'Sharpe Ratio': sharpe_ratio}
+
+    for single_portfolio in range(num_portfolios):
+        weights = np.random.random(num_assets)
+        weights /= np.sum(weights)
+        returns = np.dot(weights, returns_annual)
+        volatility = np.sqrt(np.dot(weights.T, np.dot(cov_annual, weights)))
+        port_returns.append(returns)
+        sharpe = returns / volatility
+        sharpe_ratio.append(sharpe)
+        port_volatility.append(volatility)
+        stock_weights.append(weights)
+
+    for counter,symbol in enumerate(securities):
+        portfolio[symbol+' Weight'] = [Weight[counter] for Weight in stock_weights]
+
+    df = pd.DataFrame(portfolio)
+    column_order = ['Returns', 'Volatility', 'Sharpe Ratio'] + [stock+' Weight' for stock in securities]
+    df = df[column_order]
+    return(df)
 
 def select_feature(data,feature):
     tickers = list(data.keys())
@@ -21,6 +49,15 @@ def select_feature(data,feature):
     df.columns = tickers
     return(df)
 
+def optimize_portfolio(data,op_type='max_sharpe'):
+    if op_type == 'max_sharpe':
+            max_sharpe = data['Sharpe Ratio'].max()
+            sharpe_portfolio = data.loc[data['Sharpe Ratio'] == max_sharpe]
+            return(sharpe_portfolio)
+    if op_type == 'min_variance':    
+        min_volatility = data['Volatility'].min()
+        min_variance_port = data.loc[data['Volatility'] == min_volatility]
+        return(min_variance_port.T)
 
 if __name__ == "__main__":
 
